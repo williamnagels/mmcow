952b3555ea84a6a19368443a38dfe5c11c8dc4f7
william nagels <william.nagels@gmail.com>
2017-12-20 20:16:42 +0100

    Add unittest and initial stub files

---
+++ src/cow.cpp
@@ -0,0 +1,2 @@
+#include "include/cow.h"
+V::V() {}
---
+++ src/unittest_main.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE Linker unittests
+#include <boost/test/unit_test.hpp> 
+
+BOOST_AUTO_TEST_SUITE(mmcow)
+
+
+BOOST_AUTO_TEST_CASE(simple)
+{
+	BOOST_CHECK_EQUAL(1, 1);
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
