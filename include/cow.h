bf203ab735b074f0a81f6353e8ae761cab0f1dff
william nagels <william.nagels@gmail.com>
2017-12-22 22:27:32 +0100

    Add container class & iterator type.

--- include/cow.h
+++ include/cow.h
@@ -1,6 +1,127 @@
 #pragma once
+#include <memory>
 
-class V
+template <typename T> 
+struct MM_COW;
+
+template <typename T>
+struct W_Prot
 {
-	V();
+	using iterator=W_Prot<T>;
+	using value_type = T;
+	using reference = T & ;
+	using pointer = T * ;
+	using difference_type = std::ptrdiff_t;
+	using iterator_category = std::forward_iterator_tag;
+
+	MM_COW<T>* cow;
+	T* v;
+	std::size_t idx;
+	W_Prot(T* i, std::size_t index, MM_COW<T>* coww) :
+		v(i)
+		, cow(coww)
+		, idx(index)
+	{
+
+	}
+	
+
+	T& operator*() { return *v; }
+	bool operator==(iterator const & other)
+	{
+		return idx == other.idx; 
+	}
+	bool operator!=(iterator const & other)
+	{ 
+		return !(*this == other); 
+	}
+	operator T&() const { return *v; }
+	iterator operator++()
+	{ 
+		return (*cow)[idx + 1]; 
+	}
+
+
+
+	iterator operator=(T const& other)
+	{
+		if (cow->_allocated_ptr)
+		{
+			*v = other;
+			return *this;
+		}
+		else
+		{
+			cow->allocate(cow->_current_size);
+			(*cow)[idx] = other;
+		}
+
+		*this = (*cow)[idx];
+
+		return *this;
+	}
 };
+
+template<typename T>
+struct MM_COW
+{
+	using iterator=W_Prot<T>;
+	using difference_type = ptrdiff_t;
+	using size_type=size_t ;
+	using value_type = T;
+	using pointer = T*;
+	using reference = T & ;
+
+
+	T* _ptr;
+	std::size_t  _current_size;
+	std::unique_ptr<T[]> _allocated_ptr;
+
+	MM_COW(T* ptr):
+		_ptr(ptr)
+		,_current_size(1)
+	{
+	}
+
+	MM_COW(T* start_ptr, T* end_ptr):
+		_ptr(start_ptr)
+		,_current_size(end_ptr - start_ptr)
+	{
+	}
+	 
+	iterator begin() { return iterator(_ptr, 0, this); }
+
+	iterator end() { return iterator(_ptr + (_current_size-1), _current_size-1, this); }
+
+	iterator operator[](std::size_t index)
+	{
+		if (index >= _current_size)
+		{
+			allocate(index + 1);
+		}
+		return iterator(_ptr + index, index, this);
+	}
+
+	void allocate(std::size_t new_size)
+	{
+		auto tmp_ptr = std::make_unique<T[]>(new_size);
+
+		std::memcpy(tmp_ptr.get(),_ptr, _current_size * sizeof(T));
+		_current_size = new_size;
+		_allocated_ptr = std::move(tmp_ptr);		
+		_ptr = _allocated_ptr.get();
+	}
+};
+
+/*
+template <typename T>
+typename MM_COW<T>::W_Prot begin(MM_COW<T> const& cow)
+{
+	return cow[0];
+}
+
+template <typename T>
+typename MM_COW<T>::W_Prot end(MM_COW<T> const& cow)
+{
+	return cow[cow._current_size - 1];
+}*/
--- src/unittest_main.cpp
+++ src/unittest_main.cpp
@@ -1,12 +1,109 @@
 #define BOOST_TEST_MODULE Linker unittests
 #include <boost/test/unit_test.hpp> 
+#include <include/cow.h>
 
+#include <numeric>
+#include <algorithm>
 BOOST_AUTO_TEST_SUITE(mmcow)
 
 
-BOOST_AUTO_TEST_CASE(simple)
+BOOST_AUTO_TEST_CASE(simple_uint8_t)
 {
-	BOOST_CHECK_EQUAL(1, 1);
+	uint8_t simple = 3;
+	MM_COW<uint8_t> _cow(&simple);
+	BOOST_CHECK_EQUAL(simple, 3);
+	BOOST_CHECK_EQUAL(_cow[0], 3);
+	_cow[0] = 10;
+
+	BOOST_CHECK_EQUAL(simple, 3);
+	BOOST_CHECK_EQUAL(_cow[0], 10);
+}
+
+BOOST_AUTO_TEST_CASE(simple_uint8_t_array)
+{
+	uint8_t simple[100];
+	std::iota(std::begin(simple), std::end(simple), 0);
+	
+	MM_COW<uint8_t> _cow(std::begin(simple), std::end(simple));
+
+	BOOST_CHECK_EQUAL(*simple, 0);
+	BOOST_CHECK_EQUAL(*(simple+1), 1);
+	BOOST_CHECK_EQUAL(&(uint8_t)(_cow[0]), simple);
+	BOOST_CHECK_EQUAL((uint8_t)_cow[0], 0);
+	BOOST_CHECK_EQUAL(&(uint8_t)(_cow[1]), simple+1);
+	BOOST_CHECK_EQUAL((uint8_t)_cow[1], 1);
+
+	_cow[0] = 10;
+
+	uint8_t* first_allocation_ptr = &(uint8_t)(_cow[0]);
+	BOOST_CHECK_NE(first_allocation_ptr, simple);
+	BOOST_CHECK_EQUAL((uint8_t)_cow[0], 10);
+	BOOST_CHECK_EQUAL((uint8_t)_cow[1], 1);
+	BOOST_CHECK_EQUAL((uint8_t)_cow[55], 55);
+	_cow[100] = 100;
+	_cow[55] = 88;
+	uint8_t* second_allocation_ptr = &(uint8_t)(_cow[0]);
+	BOOST_CHECK_EQUAL((uint8_t)_cow[55], 88);
+	BOOST_CHECK_EQUAL((uint8_t)_cow[100], 100);
+	BOOST_CHECK_NE(first_allocation_ptr, second_allocation_ptr);
+
+}
+
+template <class T, std::size_t size>
+struct ftor
+{
+	ftor()
+	{
+		(*this)();
+	}
+	void operator()()
+	{
+		T simple[size];
+		std::iota(std::begin(simple), std::end(simple), 0);
+
+		MM_COW<T> _cow(std::begin(simple), std::end(simple));
+
+		for (auto idx = 0; idx < size; idx++)
+		{
+			BOOST_CHECK_EQUAL(simple[idx], idx);
+			BOOST_CHECK_EQUAL(_cow[idx], simple[idx]);
+			BOOST_CHECK_EQUAL(&(T)(_cow[idx]), simple + idx);
+		}
+
+		std::reverse(std::begin(_cow), std::end(_cow));
+		_cow[0] = 10;
+		T* first_allocation_ptr = &(T)(_cow[0]);
+		for (auto idx = 0; idx < size; idx++)
+		{
+			//BOOST_CHECK_EQUAL(simple[idx], idx);
+			//BOOST_CHECK_EQUAL(_cow[idx], simple[idx]);
+			BOOST_CHECK_NE(&(T)(_cow[idx]), simple + idx);
+		}
+		BOOST_CHECK_EQUAL((T)_cow[0], 10);
+		BOOST_CHECK_EQUAL((T)_cow[1], 1);
+		BOOST_CHECK_EQUAL((T)_cow[55], 55);
+		BOOST_CHECK_EQUAL((T)_cow[13], 13);
+		BOOST_CHECK_EQUAL((T)_cow[12], 12);
+		BOOST_CHECK_EQUAL((T)_cow[11], 11);
+		_cow[100] = 100;
+		_cow[55] = 88;
+		T* second_allocation_ptr = &(T)(_cow[0]);
+		BOOST_CHECK_EQUAL((T)_cow[55], 88);
+		BOOST_CHECK_EQUAL((T)_cow[100], 100);
+		BOOST_CHECK_NE(first_allocation_ptr, second_allocation_ptr);
+	}
+};
+
+BOOST_AUTO_TEST_CASE(simple_uint64_t_array)
+{
+	ftor<uint8_t,  100>();
+	ftor<uint16_t, 100>;
+	ftor<uint32_t, 100>;
+	ftor<uint64_t, 100>;
+	ftor<int8_t, 100>;
+	ftor<int16_t, 100>;
+	ftor<int32_t, 100>;
+	ftor<int64_t, 100>;
 }
 
 
